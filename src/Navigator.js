312649bd8fa03808ad6fd010a28749ede856223c
Filip Woźniczak <f.wozniczak@ambsoft.pl>
2019-04-09 11:37:11 +0200

    fix

---
+++ src/Navigator.js
@@ -0,0 +1,27 @@
+import {createAppContainer, createStackNavigator, createSwitchNavigator} from "react-navigation";
+import SignIn from "./SignIn";
+import Home from "./Home";
+import ResetPassword from "./ResetPassword";
+
+const LoginNavigator = createStackNavigator({
+  SignIn,
+  ResetPassword
+});
+
+const AppNavigator = createStackNavigator({
+  Home
+});
+
+const Navigator = createAppContainer(
+  createSwitchNavigator(
+    {
+      Home: AppNavigator,
+      Login: LoginNavigator
+    },
+    {
+      initialRouteName: "Login"
+    }
+  )
+);
+
+export default createAppContainer(Navigator);
