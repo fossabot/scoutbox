8965d6c0239d2c612196392221c6ccaafd79897d
Filip Woźniczak <f.wozniczak@ambsoft.pl>
2019-04-09 11:35:58 +0200

    add test

--- App.test.js
+++ App.test.js
@@ -3,6 +3,11 @@ import App from './App';
 
 import renderer from 'react-test-renderer';
 
+test('renders correctly', () => {
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
 it('renders without crashing', () => {
   const rendered = renderer.create(<App />).toJSON();
   expect(rendered).toBeTruthy();
